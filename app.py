b0c51e461ae972920111ce4333012c7647cd35c7
Girafandrew <girafandrewalaniz@hotmail.com>
2023-06-01 23:45:09 -0300

    tentando upar2

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secretkey'
+app.config['SECRET_KEY'] = 'secret-key'
 socketio = SocketIO(app)
-
-# Lista para armazenar as mensagens
-messages = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/chat')
-def chat():
-    username = request.args.get('username')
-    return render_template('chat.html', username=username)
-
-@socketio.on('send_message')
-def handle_send_message_event(data):
-    messages.append(data)
-    emit('receive_message', data, broadcast=True)
-
-@socketio.on('connect')
-def handle_connect_event():
-    emit('load_messages', messages)
+@socketio.on('message')
+def handle_message(data):
+    username = data['username']
+    message = data['message']
+    timestamp = datetime.now()
+    emit('message', {'username': username, 'message': message, 'timestamp': timestamp}, broadcast=True)
 
 def start_server():
-    socketio.run(app, host='0.0.0.0', port=9000)
+    socketio.run(app, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
     start_server()
